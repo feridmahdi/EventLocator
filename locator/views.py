abb66a44e9be7f28f9832365424abd1d8c064796
Ferid Mahdi <kathorm89@gmail.com>
2022-12-17 17:23:47 +0100

    Display a map

--- locator/views.py
+++ locator/views.py
@@ -3,6 +3,7 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
 
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -16,3 +17,9 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+def map_view(request):
+    context = {
+        "google_api_key": settings.GOOGLE_API_KEY,
+    }
+    return render(request, 'locator/map.html', context)
